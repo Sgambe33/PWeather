95e37cc035d32b0b7610fda5230396cc1d9a9de0
Sgambe33 <studente.cosimo.sgambelluri@gmail.com>
2022-01-11 17:25:25 +0100

    data output options

--- showMeteo.py
+++ showMeteo.py
@@ -1,5 +1,7 @@
 # Import Meteostat library and dependencies
 from logging import exception
+import math
+from fpdf import *
 from meteostat import *
 from geopy.geocoders import *
 from datetime import *
@@ -21,9 +23,15 @@ def getPlaceFromCity(cityName):
 
 def getLocationFromIP():
     url = 'http://ipinfo.io/json'
-    response = urlopen(url)
-    ipInfo = json.load(response)
-    return ipInfo
+    while True:
+        try:
+            response = urlopen(url)
+            ipInfo = json.load(response)
+        except:
+            print("Errore di connessione...")
+            print("Ritento...")
+        finally:
+            return ipInfo
 
 def getStationIdWithIp(ipdata):
     stations = Stations()
@@ -48,24 +56,67 @@ while True:
     print("4. Esci")
     
     scelta = input()
-    
+    ipData = getLocationFromIP()
     match scelta:
         case "1":
             start = datetime.today() - timedelta(days=7)
-            end = datetime.today()
-            ipData = getLocationFromIP()          
+            end = datetime.today() 
             data = Daily(getStationIdWithIp(ipData), start, end )
             data = data.fetch() 
             print(data) 
         case "2":
-            print("null")
+            start = datetime.now() - timedelta(hours=24)
+            end = datetime.now()
+            data = Hourly(getStationIdWithIp(ipData), start, end )
+            data = data.fetch() 
         case "3":
-            print("null")
+            print("Funzionalità in corso di sviluppo. Non disponibile.")
         case "4":
-            print("null")
+            print("Data di inizio dei registri:")
+            dataS = input('Enter a date in YYYY-MM-DD format')
+            anno, mese, giorno = map(int, dataS.split('-'))
+            start = datetime.date(anno, mese, giorno)
+            #####################################################
+            dataE = input('Enter a date in YYYY-MM-DD format')
+            anno, mese, giorno = map(int, dataE.split('-'))
+            end = datetime.date(anno, mese, giorno)
+            #####################################################
+            print("Posizione: ")
+            cityName = input()
+            stations = Stations()
+            city = getPosFromCity(cityName) 
+            stations = stations.nearby(city.latitude, city.longitude)
+            station = stations.fetch(1)
+            stationId = station.filter(['id'])
+            
+            data = Monthly(stationId, start, end)
+            
+            data = data.fetch() 
+            print("In che formato salvare i dati?")
+            print("1. HTML")
+            print("2. PDF")
+            print("3. CSV")
+            print("4. JSON")
+            fileType = input()
+            match fileType:
+                case "1":
+                    data.to_html("records.html")
+                case "2":
+                    pdf = FPDF()  
+                    pdf.add_page()
+                    pdf.set_font("Arial", size = 15)
+                    f = open("myfile.txt", "r")
+                    pdf.cell(200, 10, txt = data, ln = 1, align = 'C')
+                    pdf.output("records.pdf")   
+                case "3":
+                    data.to_csv("records.csv")
+                case "4":
+                    data.to_json("records.json")
+                
         case "5":
             exit()
-    print("lOOP")
+        case _:
+            print("Opzione non valida!")
 
 
 
